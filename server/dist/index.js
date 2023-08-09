55af010526b0635fdc63305b1918d7e4a8dc387c
vikyw89 <vikyw89@gmail.com>
2023-08-09 13:30:18 +0800

    ci: changed npm script

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,24 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const express_1 = __importDefault(require("express"));
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const prisma = new client_1.PrismaClient();
-// const prisma = prismaRaw.$extends({
-//     name: 'log',
-//     query: {
-//         async $allOperations({ operation, model, args, query }) {
-//             const start = performance.now()
-//             const [result] = await prismaRaw.$transaction([query(args)]) // wrap the query in a batch transaction, and destructure the result to return an array
-//             const end = performance.now()
-//             const time = end - start
-//             console.log({ start, result, end, time })
-//             console.log({ operation, model, args, query })
-//             return result
-//         },
-//     },
-// });
+const prismaRaw = new client_1.PrismaClient();
+const prisma = prismaRaw.$extends({
+    name: 'activity',
+    query: {
+        async $allOperations({ operation, model, args, query }) {
+            const start = performance.now();
+            const [result] = await prismaRaw.$transaction([query(args)]); // wrap the query in a batch transaction, and destructure the result to return an array
+            const end = performance.now();
+            const time = end - start;
+            console.log({ start, result, end, time });
+            console.log({ operation, model, args, query });
+            return result;
+        },
+    },
+});
 // ... your REST API routes will go here
 app.get('/users', async (req, res) => {
     const users = await prisma.user.findMany();
@@ -37,4 +37,4 @@ app.post('/users', async (req, res) => {
     });
     res.json(users);
 });
-app.listen(port, () => console.log('REST API server ready at: http://localhost:3000'));
+app.listen(port, () => console.log(`REST API server ready at: http://localhost:${port}`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,21 +5,21 @@ const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.json())
-const prisma = new PrismaClient()
-// const prisma = prismaRaw.$extends({
-//     name: 'log',
-//     query: {
-//         async $allOperations({ operation, model, args, query }) {
-//             const start = performance.now()
-//             const [result] = await prismaRaw.$transaction([query(args)]) // wrap the query in a batch transaction, and destructure the result to return an array
-//             const end = performance.now()
-//             const time = end - start
-//             console.log({ start, result, end, time })
-//             console.log({ operation, model, args, query })
-//             return result
-//         },
-//     },
-// });
+const prismaRaw = new PrismaClient()
+const prisma = prismaRaw.$extends({
+    name: 'activity',
+    query: {
+        async $allOperations({ operation, model, args, query }) {
+            const start = performance.now()
+            const [result] = await prismaRaw.$transaction([query(args)]) // wrap the query in a batch transaction, and destructure the result to return an array
+            const end = performance.now()
+            const time = end - start
+            console.log({ start, result, end, time })
+            console.log({ operation, model, args, query })
+            return result
+        },
+    },
+});
 
 
 // ... your REST API routes will go here
